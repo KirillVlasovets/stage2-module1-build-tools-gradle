d721097fbc64135266344de565ee57ec7689ce89
Kirill Vlasovets <k.vlasovets@a1qa.com>
2024-01-18 03:53:20 +0400

    fixes added

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,12 +2,12 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if (str.contains("0")) {
-            return false;
-        }
         double number;
         try {
             number = Double.parseDouble(str);
+            if (str.startsWith("0")) {
+                return false;
+            }
         } catch (NumberFormatException | NullPointerException exception) {
             return false;
         }
