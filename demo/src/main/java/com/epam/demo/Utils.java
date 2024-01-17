78ce177f103328130e616874358419e1d6c3a39b
Kirill Vlasovets <k.vlasovets@a1qa.com>
2024-01-18 03:47:31 +0400

    fixes added

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -6,6 +6,9 @@ public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         for (var srt : args) {
             double number;
+            if (srt.contains("0")) {
+                return false;
+            }
             try {
                 number = Double.parseDouble(srt);
             } catch (NumberFormatException | NullPointerException exception) {
